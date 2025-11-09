aaa0cd72f6615edaf6704cd8f4cc460089735cc5
KateyKwesi <kateykwesi001@gmail.com>
2025-11-09 10:22:33 +0000

    initial commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer style={{ color: "white" }}>
+    <p>© 2023 City Lovers</p>
+  </footer>
+);
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+const MainContent = () => (
+  <main style={{ color: "white" }}>
+    <p>I love to visit New York, Paris, and Tokyo.</p>
+  </main>
+);
+
+export default MainContent;
