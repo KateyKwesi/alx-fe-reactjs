f02fdc155ec0e038767da0a81f08521805267f36
kateykwesi <kateykwesi001@gmail.com>
2025-12-13 12:03:26 +0100

    initial commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./components/Profile";
 import Post from "./components/Post";
+import BlogPost from "./components/BlogPost";
 
 function App() {
   const isAuthenticated = true;
@@ -19,6 +20,8 @@ function App() {
         />
 
         <Route path="/post/:id" element={<Post />} />
+
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
